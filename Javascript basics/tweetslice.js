38307fe586b16f5751bdfb9c7a76cd58bc80fff2
harshavardham07 <mharshaavardhann@gmail.com>
2024-05-10 17:40:41 +0530

    Js

---
+++ Javascript basics/tweetslice.js
@@ -0,0 +1,11 @@
+
+var tweet =prompt("Enter your tweet");
+var tweetCount = tweet.length;
+
+ alert("The length of the tweet is" + tweetCount + "Remining chracters is " + (180 - tweetCount));
+ var short=tweet.slice(0,140);
+ alert(short);
+
+ var name = " harsha";
+ name=name.toUpperCase();
+ name = name.toLowerCase();
